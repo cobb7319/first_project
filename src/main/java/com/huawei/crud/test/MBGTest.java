204ce9095a461a8e553e2b055e3698516dad3bf0
tangsijian <jsiqi0810@sina.com>
2018-12-09 12:32:52 +0800

    123

---
+++ src/main/java/com/huawei/crud/test/MBGTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.huawei.crud.test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * <p>Title: MBGTest</p>
+ * <p>Description: </p>
+ * @author E-mail: jsiqi0810@sina.com
+ * @date 2018年11月10日
+ */
+public class MBGTest {
+	public static void main(String[] args) throws Exception{
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("mbg.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+}
